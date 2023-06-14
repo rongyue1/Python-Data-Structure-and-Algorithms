f2d7c6ee9593c03d63df6fb7da4b61942c4e04e9
Rong Sun <sun.rongyue@outlook.com>
2023-06-14 15:53:37 +1200

    finish append method

--- Python Data Structure & Algorithms/Linked List/EXERCISE-LL-Append.py
+++ Python Data Structure & Algorithms/Linked List/EXERCISE-LL-Append.py
@@ -24,12 +24,12 @@ class LinkedList:
         
     def append(self, value):
         new_node = Node(value)
-        
-        #################################
-        # FINISH WRITING APPEND METHOD  #
-        # INSERT IF/ELSE STATEMENT HERE #
-        #################################
-        
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
         self.length += 1
 
 
