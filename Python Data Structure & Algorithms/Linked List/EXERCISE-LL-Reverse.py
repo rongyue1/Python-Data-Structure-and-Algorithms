9628996d32c3b6c55c24c1d4c0a3908246fb496a
Rong Sun <sun.rongyue@outlook.com>
2023-06-15 21:48:24 +1200

    finish remove and reverse method

--- Python Data Structure & Algorithms/Linked List/EXERCISE-LL-Reverse.py
+++ Python Data Structure & Algorithms/Linked List/EXERCISE-LL-Reverse.py
@@ -109,12 +109,18 @@ class LinkedList:
         self.length -= 1
         return temp
 
-    ## WRITE REVERSE METHOD HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
   
 
 
